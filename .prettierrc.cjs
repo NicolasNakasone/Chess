4707dec3cd23b472ec6adf2b33f722577f206438
Nicolas Nakasone <nicolasnakasone70@gmail.com>
2023-11-26 03:31:40 -0300

    feat(config): initial configs

---
+++ .prettierrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  tabWidth: 2,
+  arrowParens: 'avoid',
+  singleQuote: true,
+  semi: false,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  endOfLine: 'lf',
+  printWidth: 99,
+}
